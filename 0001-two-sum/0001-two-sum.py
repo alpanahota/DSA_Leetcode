2e198c9b0fa3743b7ff82345b44ced6be23e089c
alpanahota <alpanahota@gmail.com>
2026-01-02 23:22:38 +0530

    Time: 3 ms (56.13%), Space: 13.4 MB (16%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,9 +1,15 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        n = len(nums)
-        for i in range(n):
-            for j in range(i + 1, n):  # Start j from i+1 to avoid duplicate pairs and checking same element
-                if nums[i] + nums[j] == target:
-                    return [i, j]
-        return []  # No such pair found
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        seen = {}
+        for i in range(len(nums)):
+            need = target - nums[i]
+            if need in seen:
+                return [seen[need],i]     
+            seen[nums[i]] = i      
+                                        
         
